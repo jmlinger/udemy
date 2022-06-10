215dd6f803d2a424b867c8a886c90d2d8459e284
jmlinger <jmlinger10@gmail.com>
2022-06-09 22:47:05 -0300

    feat: implementa validação ao arquivo principal.

--- nest_do_zero/initial_nest/src/main.ts
+++ nest_do_zero/initial_nest/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, //tipa o objeto do corpo da requisição conforme definido no dto.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
